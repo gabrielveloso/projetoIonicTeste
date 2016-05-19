c7e9cf800fc4776d78bcbc3cd5ecc1b7f7c5cc2a
Gabriel Azuirson <gabriel.azuirson@ufpe.br>
2016-05-19 18:08:37 -0300

    tela perfil mostrando foto

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,9 @@
 //ensinando a cadastrar um formulario de contatos
 //https://www.youtube.com/watch?v=wkhClyW5uMc
 
+// resize camera
+//https://forum.ionicframework.com/t/camera-image-resize-compression/13387
+
 angular.module('app.controllers', ['firebase', 'ngCordova'])
 
 
@@ -33,6 +36,8 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
    //$scope.usuario = $firebaseArray(ref);
     //var fbAuth = ref.getAuth();
 
+
+
     var usersRef = ref.child("usuarios");
    $scope.addUsuario = function(){       
        $scope.imagem = ($scope.imgURI !==null) ? $scope.imgURI : null;    
@@ -52,6 +57,26 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
       } else {
         console.log('Synchronization succeeded');
       }
+  };
+
+  $scope.takePhoto = function () {
+        var options = {
+          quality: 75,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 300,
+          targetHeight: 300,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false
+      };
+
+      $cordovaCamera.getPicture(options).then(function (imageData) {
+          $scope.imgURI = "data:image/jpeg;base64," + imageData;
+      }, function (err) {
+          // An error occured. Show a message to the user
+      });
   };
 
 })
@@ -91,6 +116,7 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
         $scope.email = pessoa.email;
         $scope.descricao = pessoa.descricao;
         $scope.senha = pessoa.senha;
+        $scope.foto = pessoa.foto;
     }); 
     
 });
